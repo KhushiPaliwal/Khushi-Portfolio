e0fb527f75cc119710677e1d27bb80bd78b17ec5
KhushiPaliwal <khushi19012000@gmail.com>
2025-04-23 18:27:05 +0530

    ✨ First commit: Khushi’s Portfolio uploaded

---
+++ script.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+    $("div.wrapper div#main div#index").show();
+    $("div.wrapper div#main div#acads").hide();
+    $("div.wrapper div#main div#works").hide();
+    $("div.wrapper div#main div#extra").hide(); 
+    
+    
+    $("div.wrapper div#main div#menu a.index").click(function(){
+        $("div.wrapper div#main div#picture").show();
+        $("div.wrapper div#main div#index").show();
+        $("div.wrapper div#main div#acads").hide();
+        $("div.wrapper div#main div#works").hide();
+        $("div.wrapper div#main div#extra").hide();
+    });
+    
+    $("div.wrapper div#main div#menu a.acads").click(function(){
+        $("div.wrapper div#main div#picture").show();
+        $("div.wrapper div#main div#index").hide();
+        $("div.wrapper div#main div#acads").show();
+        $("div.wrapper div#main div#works").hide();
+        $("div.wrapper div#main div#extra").hide();
+    });
+    
+    $("div.wrapper div#main div#menu a.works").click(function(){
+        $("div.wrapper div#main div#picture").show();
+        $("div.wrapper div#main div#index").hide();
+        $("div.wrapper div#main div#acads").hide();
+        $("div.wrapper div#main div#works").show();
+        $("div.wrapper div#main div#extra").hide();
+    });
+    
+    $("div.wrapper div#main div#menu a.extra").click(function(){
+        $("div.wrapper div#main div#picture").show();
+        $("div.wrapper div#main div#index").hide();
+        $("div.wrapper div#main div#acads").hide();
+        $("div.wrapper div#main div#works").hide();
+        $("div.wrapper div#main div#extra").show();
+    
+    });
+    
+    });
